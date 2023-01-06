b78d6d9a24fd727a1d80e72086889b235bf7a84c
Saad Nauman <87758531+saaaadi16@users.noreply.github.com>
2023-01-06 17:54:45 +0500

    Added reducers

--- src/context/user/user.tsx
+++ src/context/user/user.tsx
@@ -1,8 +1,35 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
 } from "../../utils/firebase/firebase";
+
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state: any, action: any) => {
+  console.log("dispatched");
+
+  console.log(action);
+
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state, // Spread through previous state
+        currentUser: payload,
+      };
+    case "increment":
+      return {
+        value: state.value + 1,
+      };
+
+    default:
+      throw new Error(`Unhandled type ${type}`);
+  }
+};
 
 // ? As the actual value you want to access
 export const UserContext: any = createContext({
@@ -10,9 +37,24 @@ export const UserContext: any = createContext({
   setCurrentUser: () => null,
 });
 
+const INITIAL_USER_STATE = {
+  currentUser: null,
+};
+
 // ? component that wraps other components that need that info
 export const UserProvider = ({ children }: any) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+
+  const [{ currentUser }, dispatch] = useReducer(
+    userReducer,
+    INITIAL_USER_STATE
+  );
+  console.log(currentUser);
+
+  const setCurrentUser = (user: any) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
@@ -34,3 +76,14 @@ export const UserProvider = ({ children }: any) => {
     </>
   );
 };
+
+/**
+ * const userReducer = (state, action) => {
+ * return {
+ *  currentUser: null,
+ *  setCurrentUser: () => {}
+ * }
+ *
+ * }
+ *
+ */
