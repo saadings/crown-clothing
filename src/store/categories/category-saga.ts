475cb7f0f4790525ecd7533df43939e46deb2a2e
Saad Nauman <saadijutt007@gmail.com>
2023-09-21 23:40:48 +0500

    updated to redux saga branch

---
+++ src/store/categories/category-saga.ts
@@ -0,0 +1,29 @@
+import { takeLatest, all, call, put } from "redux-saga/effects";
+
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase";
+import {
+  fetchCategoriesSuccess,
+  fetchCategoriesFailure,
+} from "./category-action";
+
+import { CATEGORIES_ACTIONS_TYPES } from "./category-types";
+
+export function* fetchCategoriesAsync(): any {
+  try {
+    const categoriesArray = yield call(getCategoriesAndDocuments, "categories");
+    yield put(fetchCategoriesSuccess(categoriesArray));
+  } catch (error) {
+    yield put(fetchCategoriesFailure(error));
+  }
+}
+
+export function* onFetchCategories() {
+  yield takeLatest(
+    CATEGORIES_ACTIONS_TYPES.FETCH_CATEGORIES_START,
+    fetchCategoriesAsync
+  );
+}
+
+export function* categoriesSaga() {
+  yield all([call(onFetchCategories)]);
+}
